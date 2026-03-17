cbe2a31d3c42f62d8ea922b6173378e63b6cb082
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-03-16 21:37:01 -1200

    use client componet

--- app/about/misson/page.js
+++ app/about/misson/page.js
@@ -1,5 +1,17 @@
+"use client";
 import React from "react";
 
 export default function Misson() {
-  return <div>Misson Page</div>;
+  return (
+    <div>
+      <div>Misson page</div>
+
+      <button
+        onClick={() => console.log("I have click hered")}
+        className="mt-3 p-2 bg-green-500 "
+      >
+        Click Here
+      </button>
+    </div>
+  );
 }
