539f46b304d535f721d65c609e9a0de6cec9e52d
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-03-16 20:33:05 -1200

    learn not found error

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 export default async function Blog({ params }) {
   const { id } = await params;
+  if (id === "3") {
+    notFound();
+  }
   return <div>This is : {id}</div>;
 }
---
+++ app/blogs/not-found.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div>The Requested blog was not found! </div>;
+}
