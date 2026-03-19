fd664bd0a3a85fa291d47565a24b15ef0204230f
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-03-18 21:48:09 -1200

    use Image tag in next.js

--- app/about/misson/page.js
+++ app/about/misson/page.js
@@ -1,12 +1,17 @@
-"use client";
 import Button from "@/app/components/Button";
+import Image from "next/image";
 import React from "react";
+import pimg from "@/public/images/img.jpg";
 
 export default function Misson() {
   return (
     <div>
       <div>Misson page</div>
       <h3>lorem4aldjfaslkjflajfoejfoad</h3>
+
+      <div className="w-[400]">
+        <Image src={pimg} alt="p=img" />
+      </div>
 
       <Button />
     </div>
