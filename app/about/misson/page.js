0d3aee37be9864f5dca3ff153e79601398539337
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-03-18 21:11:59 -1200

    how to use server component under client component

--- app/about/misson/page.js
+++ app/about/misson/page.js
@@ -1,17 +1,14 @@
 "use client";
+import Button from "@/app/components/Button";
 import React from "react";
 
 export default function Misson() {
   return (
     <div>
       <div>Misson page</div>
+      <h3>lorem4aldjfaslkjflajfoejfoad</h3>
 
-      <button
-        onClick={() => console.log("I have click hered")}
-        className="mt-3 p-2 bg-green-500 "
-      >
-        Click Here
-      </button>
+      <Button />
     </div>
   );
 }
---
+++ app/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <button
+      onClick={() => console.log("I have click hered")}
+      className="mt-3 p-2 bg-green-500 "
+    >
+      Click Here
+    </button>
+  );
+}
